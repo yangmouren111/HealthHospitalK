aed5b9998206b91c72f1272d6cd573b7bbe008e1
my_name <1018423878@qq.com>
2019-12-04 09:06:03 +0800

    first commit

---
+++ health_main/src/main/java/com/bawei/health_main/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bawei.health_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dingtao.common.core.WDActivity;
+
+public class MainActivity extends WDActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+}
