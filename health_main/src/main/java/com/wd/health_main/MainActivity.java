3ca4d79d396a29fafddd467e434df90f6a2a57a8
bawang <2690925254@qq.com>
2019-12-04 15:23:38 +0800

    12.04

--- health_main/src/main/java/com/wd/health_main/MainActivity.java
+++ health_main/src/main/java/com/wd/health_main/MainActivity.java
@@ -1,8 +1,30 @@
 package com.wd.health_main;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import com.wd.common.core.WDActivity;
 
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends WDActivity {
+
+
+    @BindView(R2.id.main_pager)
+    ViewPager mainPager;
+    @BindView(R2.id.main_btn_home)
+    RadioButton mainBtnHome;
+    @BindView(R2.id.main_btn_cicle)
+    RadioButton mainBtnCicle;
+    @BindView(R2.id.main_btn_video)
+    RadioButton mainBtnVideo;
+    @BindView(R2.id.main_group)
+    RadioGroup mainGroup;
 
     @Override
     protected int getLayoutId() {
@@ -18,4 +40,24 @@ public class MainActivity extends WDActivity {
     protected void destoryData() {
 
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R2.id.main_btn_home, R2.id.main_btn_cicle, R2.id.main_btn_video})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R2.id.main_btn_home:
+                break;
+            case R2.id.main_btn_cicle:
+                break;
+            case R2.id.main_btn_video:
+                break;
+        }
+    }
+
 }
