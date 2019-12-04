cbb80accdc7ebb35d8529c5596a97e17034b3551
my_name <1018423878@qq.com>
2019-12-04 09:58:15 +0800

    12.4 9:50   包名更正

---
+++ health_main/src/main/java/com/wd/health_main/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wd.health_main;
+
+import com.wd.common.core.WDActivity;
+
+public class MainActivity extends WDActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+}
